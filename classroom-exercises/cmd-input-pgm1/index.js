5681f8aa5ad733bb1aae19405372c931f8d201b0
Anil Raj <anilraje122@gmail.com>
2020-08-24 10:05:25 +0530

    Add cmd input pgm part of classroom-exercises

---
+++ classroom-exercises/cmd-input-pgm1/index.js
@@ -0,0 +1,4 @@
+var readlineSync = require('readline-sync');
+var a = Number(readlineSync.question("Enter Your First Number :"));
+var b = Number(readlineSync.question("Enter Your Second Number :"));
+console.log(a+b);
