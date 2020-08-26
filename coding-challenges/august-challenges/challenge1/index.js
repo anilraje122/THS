4fd0205d0e365d679ba5419ceeaf91f470aa030a
Anil Raj <anilraje122@gmail.com>
2020-08-26 15:50:09 +0530

    Add more updates to the august challenge1 code

--- coding-challenges/august-challenges/challenge1/index.js
+++ coding-challenges/august-challenges/challenge1/index.js
@@ -1,19 +1,27 @@
-/*****
+/**********************************************
 
 Given an array of positive integer elements.Print out the count of Prime
 numbers and Narcissistic numbers from the array and remove those elements from the
 array.
 
-Prime number : A number that is divisible only by itself and 1
+Sample Input 1 :
+10,20,30,40,2,5,7,153,1024,1634,11
+
+Expected Output format :
+Prime Numbers Count : 4
+Narcissistic Numbers Count : 5
+The updated array : 10,20,30,40,1024
+
+What is Prime number : A number that is divisible only by itself and 1
 Eg: 2, 3, 5, 7, 11
 
-Narcissistic number : An n-digit number that is the sum of the nth powers of its digits is called an n-narcissistic number.
+What is Narcissistic number : An n-digit number that is the sum of the nth powers of its digits is called an n-narcissistic number.
 Eg: 370 = 3^3 + 7^3 + 0^3. here 3 is length of the input number
 Eg: 1634 = 1^4 + 6^4 + 3^4 + 4^4. here 4 is length of the input number
 
 How to run this program : npm run challenge1
 
-*****/
+***********************************************/
 
 isPrime = (num) => {
     // prime number series starts with 2
@@ -62,9 +70,6 @@ countAndRemove = (inputArr) => {
     let narcissisticCount = 0;
     let eleToRemArr = [];
     
-    // print original input array
-    console.log(`\nInput array : ${inputArr}`);
-
     for(let i=0; i<inputArr.length; i++) {
         if(isPrime(inputArr[i])) {
             primeCount++; // counting all prime and narcissistic numbers
@@ -82,9 +87,9 @@ countAndRemove = (inputArr) => {
     });
     
     // printing outputs
-    console.log(`Number of prime numbers : ${primeCount}`);
-    console.log(`Number of narcissistic numbers : ${narcissisticCount}`);
-    console.log(`Array without prime and narcissistic numbers : ${newArr}`);
+    console.log(`\nPrime Numbers Count : ${primeCount}`);
+    console.log(`Narcissistic Numbers Count : ${narcissisticCount}`);
+    console.log(`The updated array : ${newArr}`);
 }
 
 countAndRemove(getInput());
