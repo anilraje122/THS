714f8e39e92723370482f9b53ab45372c805d098
Anil Raj <anilraje122@gmail.com>
2020-08-29 15:38:46 +0530

    Add challenge13 to august-challenges

---
+++ coding-challenges/august-challenges/challenges/algo-challenges/challenge13/index.js
@@ -0,0 +1,33 @@
+/**********************************************
+
+Write An Algorithm to check if a given number is in the following series:
+4,16,64,256,1024,4096,16384.,......., 4^N
+Note : Output will be yes/no
+
+***********************************************/
+
+// get user inputs and display the output
+main = () => {
+    let readLineSync = require('readline-sync');
+    console.log("\n-- Crack the Series --")
+    let n = readLineSync.question("\nEnter a number : ");
+    checkNum(n)?console.log('\nYes'):console.log('\nNo'); // print yes if checkNum returns true
+}
+
+// check if the input number is part of ther series
+checkNum = (n) => {
+    let isValid = false;
+
+    while(n) {
+        if(n>0 && n/4 === 1) { // confirm that input is a positive integer and quotient of input and 4 is 1
+            isValid = true;
+        }
+        n /=4; // lowering the n value to check until 1
+    }
+    return isValid;
+}
+
+main();
+
+
+
