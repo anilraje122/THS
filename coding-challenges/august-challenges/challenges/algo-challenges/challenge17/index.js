b6e5d6ec8bb3b5daac8eb2dff84624460cae24af
Anil Raj <anilraje122@gmail.com>
2020-08-29 19:04:19 +0530

    Add challenge17 to august-challenges

---
+++ coding-challenges/august-challenges/challenges/algo-challenges/challenge17/index.js
@@ -0,0 +1,30 @@
+/**********************************************
+
+Crack the series 2 15 41 80 132 197 275 366 470 587 . . .
+
+B. Write an algorithm to produce the above number series until ‘N’ where N is a
+positive integer.
+
+Note : Input N should be taken as command line input.
+
+Sample Input : 50
+Sample Output: 2 15 41
+
+
+***********************************************/
+
+// get user inputs and display the output
+main = () => {
+    let readLineSync = require('readline-sync');
+    console.log("\n-- Crack the Series 1 --")
+    let n = readLineSync.question("\Enter a number : ");
+    const const13 = 13;
+    let sum = 2;
+    for(let i=0; i<=n; i++) {
+        sum += (const13 * i); // sum = sum + (13 * i) where i = 0 to n
+        if(sum>=n) { return; }
+        console.log(sum);
+    }
+}
+
+main();
