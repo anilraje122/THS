ea2de598997f60e64f779246dd6d2262f5c1bcea
Anil Raj <anilraje122@gmail.com>
2020-09-18 12:19:39 +0530

    Add both ES5 and ES6 versions of algo challenges

---
+++ algo-ds-challenges/challenge-7-ES5.js
@@ -0,0 +1,20 @@
+
+/******
+ Write a program that prints the numbers from 1 to 100.
+ For multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz"
+ ******/
+// ES5
+
+function fizzBuzz() {
+    for(var i=1; i<=100; i++) {
+        if(i%3 == 0) {
+            console.log(`Fizz`);
+        } else if(i%5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+ }
+
+ fizzBuzz();
--- algo-ds-challenges/challenge-7-ES6.js
+++ algo-ds-challenges/challenge-7-ES6.js
@@ -3,9 +3,10 @@
  Write a program that prints the numbers from 1 to 100.
  For multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz"
  ******/
+// ES6
 
- function fizzBuzz() {
-    for(var i=1; i<=100; i++) {
+fizzBuzz = () => {
+    for(let i=1; i<=100; i++) {
         if(i%3 == 0) {
             console.log(`Fizz`);
         } else if(i%5 == 0) {
