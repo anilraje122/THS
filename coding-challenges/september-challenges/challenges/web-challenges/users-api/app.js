146895ef8684d592cb1cf3ee15e542cb583e37c5
Anil Raj <anilraje122@gmail.com>
2020-09-28 11:42:52 +0530

    Implement POST method in users-api challenge

---
+++ coding-challenges/september-challenges/challenges/web-challenges/users-api/app.js
@@ -0,0 +1,63 @@
+// Imports
+const http = require('http');
+const url = require('url');
+const stringDecoder = require('string_decoder').StringDecoder;
+const helpers = require('./lib/helpers');
+const config = require('./config');
+const handlers = require('./lib/handlers');
+const { stat } = require('fs');
+
+// Create HTTP Server
+const httpServer = http.createServer((req, res) => {
+    // Get data from the request
+    const parsedUrl = url.parse(req.url, true);
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headers = req.headers;
+
+    // Get the payload(body) from the request
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Construct the data object
+        const data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload: helpers.parseJsonToObject(buffer)
+        }
+
+        // Choose the handler where request go according to the route path
+        const chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Route the request to the handler that we chosen
+        chosenHandler (data, (statusCode, payload) => {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+            payload = typeof (payload) == 'object' ? payload : {};
+            payloadString = JSON.stringify(payload);
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
+    });
+});
+
+httpServer.listen(config.httpPort, () => {
+    console.log(`Server listening on ${config.httpPort}`);
+});
+
+// Implementing Router
+const router = {
+    'ping': handlers.ping,
+    'users': handlers.users
+}
+
+
+
