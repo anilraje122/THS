5068da810bb0eb4a2fa09f19d4f3a89f2febca41
Anil Raj <anilraje122@gmail.com>
2020-10-03 22:18:30 +0530

    Add file-system practice codes

---
+++ classroom-exercises/homeworks/core-nodejs-practice/file-system-v2/index.js
@@ -0,0 +1,25 @@
+const fslib = require('./lib/fslib');
+
+// File create and write
+
+// fslib.create('.data', 'testfile', {"Name": "Anil", "Email": "anil@gmail.com"}, (err, msg) => {
+//     (!err) ? console.log(msg) : console.log(msg, err);
+// });
+
+// File read
+
+// fslib.read('.data', 'testfile', (err, data) => {
+//     (!err) ? console.log(`Info: File content is ${data}`) : console.log('Error: Unable to read file!\n', err);
+// });
+
+// File update (replace content)
+
+// fslib.update('.data', 'testfile', {"Name": "Anil Raj", "Email": "anilraj@gmail.com"}, (err, msg) => {
+//     (!err) ? console.log(msg) : console.log(msg, err);
+// });
+
+// File delete
+
+// fslib.delete('.data', 'testfile', (err, msg) => {
+//     (!err) ? console.log(msg) : console.log(msg, err);
+// });
