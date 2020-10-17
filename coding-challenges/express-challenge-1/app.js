a8bd34da253ee35ab34804db6a241b67e827c992
Anil Raj <anilraje122@gmail.com>
2020-10-17 21:22:44 +0530

    Add SSL config to express-challenge-1

--- coding-challenges/express-challenge-1/app.js
+++ coding-challenges/express-challenge-1/app.js
@@ -3,13 +3,18 @@ const express = require('express');
 const fs = require('fs');
 const _data = require('./store/data');
 const serveIndex = require('serve-index');
-
+const https = require('https');
 
 /* Global vars */
 const app = express();
 const port = process.env.PORT || 3000;
 const baseDir = 'logs';
+let key = fs.readFileSync('.ssl/private.key');
+let cert = fs.readFileSync('.ssl/certificate.crt');
+let ca = fs.readFileSync('.ssl/ca_bundle.crt');
+const server = https.createServer({key: key, cert: cert, ca: ca}, app);
 
+/* Server static files under public folder */
 app.use('/.well-known/pki-validation/', serveIndex('public/.well-known/pki-validation/'), express.static('public/.well-known/pki-validation/'));
 
 /* Log IP for every request */
@@ -43,4 +48,4 @@ app.get('/', (req, res) => {
 })
 
 /* HTTP server listening on port specified */
-app.listen(port, () => console.log(`Server listening on ${port}`));
+server.listen(port, () => console.log(`Server listening on ${port}`));
