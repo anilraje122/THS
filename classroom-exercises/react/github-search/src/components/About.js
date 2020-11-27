5b62115056daa6984cb58ba6a6df7d7356b14bbe
Anil Raj <anilraje122@gmail.com>
2020-11-27 21:04:21 +0530

    Init express-user-api project

--- classroom-exercises/react/github-search/src/components/About.js
+++ classroom-exercises/react/github-search/src/components/About.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-function About() {
-  return (
-    <>
-      <h1>About Github Search Engine</h1>
-      <p>This app is build using React JS</p>
-    </>
-  );
+class About extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      fname: "",
+      lname: "",
+      password: "",
+    };
+  }
+  render() {
+    return (
+      <>
+        <h1>React Forms</h1>
+        <p>This is s form submission in React and Express</p>
+        <div className="card">
+          <form action="/action_page.php">
+            <label for="fname">First name</label>
+            <input type="text" name="fname" />
+            <label for="lname">Last name</label>
+            <input type="text" name="lname" />
+            <label for="password">Password</label>
+            <input type="password" name="password" />
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default About;
